903f613d7ea227b9d8a2d848a8f9360336ee8fe8
fzarate <fede.zarate.uni@gmail.com>
2022-03-10 23:59:27 -0600

    CRUD IMAGES; this feature was added to save a delete images in cloudinary

--- src/config.js
+++ src/config.js
@@ -5,3 +5,6 @@ if(process.env.NODE_ENV !== 'production'){
 
 module.exports.MONGODB_URI = process.env.MONGODB_URI;
 module.exports.PORT = process.env.PORT;
+module.exports.CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+module.exports.CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+module.exports.CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
